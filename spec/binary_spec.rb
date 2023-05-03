b62db0fcadc4be15a67881c54929d6a7032af8f1
Jon Widener <jon.widener@daveramsey.com>
2023-05-03 13:26:19 -0500

    Fixes some indentations

--- spec/binary_spec.rb
+++ spec/binary_spec.rb
@@ -48,18 +48,18 @@ RSpec.describe Binary do
   end
 
   context 'OR' do
-  (1..10).each do |i|
-    length = rand(2..4)
-    args = []
-    (0...length).each do |j|
-      args << rand(0..100) 
-    end
-    answer = args.reduce { |el, res| el | res }
-    it "[#{args.map { |el| el.to_s 2}.join(', ')}]: #{answer.to_s 2}" do
-      expect(Binary.or(*args.map { |arg| arg.to_s(2) })).to eq answer.to_s(2)
+    (1..10).each do |i|
+      length = rand(2..4)
+      args = []
+      (0...length).each do |j|
+        args << rand(0..100) 
+      end
+      answer = args.reduce { |el, res| el | res }
+      it "[#{args.map { |el| el.to_s 2}.join(', ')}]: #{answer.to_s 2}" do
+        expect(Binary.or(*args.map { |arg| arg.to_s(2) })).to eq answer.to_s(2)
+      end
     end
   end
-end
  
 end
 
